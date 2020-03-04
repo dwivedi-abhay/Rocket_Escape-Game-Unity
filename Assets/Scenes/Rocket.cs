5c8e5b78191712ba278993a4f568b154d3b4c6d0
Abhay <abhaydwivedi230@gmail.com>
2020-03-05 00:00:36 +0530

    Rocket Thrust Added

--- Assets/Scenes/Rocket.cs
+++ Assets/Scenes/Rocket.cs
@@ -7,12 +7,15 @@ public class Rocket : MonoBehaviour
 {
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+        
     }
 
     // Update is called once per frame
@@ -26,6 +29,17 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            
+
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+
+        else
+        {
+            audioSource.Stop();
         }
 
         if (Input.GetKey(KeyCode.A))
